a08d744fe207bd54dd4401c47cb6bd588958c4cb
nafi-rahman <nafirahman16@gmail.com>
2022-09-11 04:16:48 +0600

    added comments

--- cnnmnist.py
+++ cnnmnist.py
@@ -4,24 +4,26 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-print(tf.__version__)
+
+#loads the data set into train and test
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+#normalize the data so it is in a range from 0 to 1
 x_train = x_train/255.0
 x_test = x_test/255.0
 
+#converts the training labels into a one-hot vector
 y_train = tf.one_hot(y_train.astype(np.int32),depth=10)
 y_test = tf.one_hot(y_test.astype(np.int32),depth=10)
 
-plt.imshow(x_train[100])
-print(y_train[100])
-
+#variables for the model
 input_shape = (28, 28, 1)
 batch_size = 64
 num_classes = 10
 epochs = 5
 
+#build the model
 model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(32, (5,5), padding='same', activation='relu', input_shape=input_shape),
     tf.keras.layers.Conv2D(32, (5,5), padding='same', activation='relu'),
@@ -36,9 +38,10 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(num_classes, activation='softmax')
 ])
-
+#compile the model
 model.compile(optimizer=tf.keras.optimizers.RMSprop(epsilon=1e-08), loss='categorical_crossentropy', metrics=['acc'])
 
+#fucntion to stop the model wehn it reaches a certain accuracy
 class myCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs={}):
     if(logs.get('acc')>0.995):
@@ -47,12 +50,14 @@ class myCallback(tf.keras.callbacks.Callback):
 
 callbacks = myCallback()
 
+#train the model
 history = model.fit(x_train, y_train,
                     batch_size=batch_size,
                     epochs=epochs,
                     validation_split=0.1,
                     callbacks=[callbacks])
-
+                    
+#evaluate the model / test the model with the test data
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
